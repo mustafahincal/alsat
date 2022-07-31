c87ec7d12a12cc0fd75607a002344a91e2397efd
mustafahincal <mustafahncal@gmail.com>
2022-07-31 13:16:59 +0300

    update

--- project-server/Core/DependencyResolvers/CoreModule.cs
+++ project-server/Core/DependencyResolvers/CoreModule.cs
@@ -1,5 +1,4 @@
-﻿using Core.CrossCuttingConcerns.Caching;
-using Core.CrossCuttingConcerns.Caching.Microsoft;
+﻿
 using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,8 +17,6 @@ namespace Core.DependencyResolvers
         {
             serviceCollection.AddMemoryCache();
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
-            serviceCollection.AddSingleton<Stopwatch>();
         }
 
     }
