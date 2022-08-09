827eca586c83880e8080495408abe91d730de720
mustafahincal <mustafahncal@gmail.com>
2022-08-09 18:02:45 +0300

    update AddProduct control image service

--- project-client/src/validations/productSchema.js
+++ project-client/src/validations/productSchema.js
@@ -2,10 +2,10 @@ import Yup from "./validation";
 
 export const ProductSchema = Yup.object().shape({
   name: Yup.string().required().max(100),
-  categoryId: Yup.string().required().max(500),
+  categoryId: Yup.string().required(),
   brandId: Yup.string(),
   colorId: Yup.string(),
   price: Yup.number().required(),
-  description: Yup.string().required(),
+  description: Yup.string().required().max(500),
   usingStateId: Yup.string().required(),
 });
