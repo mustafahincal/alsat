e60fdb0724e4236b6cd23edc13ff37e31cea3d81
mustafahincal <mustafahncal@gmail.com>
2022-08-10 17:53:36 +0300

    create changePasswordDto, update authcontrollers

---
+++ project-server/Core/Entities/Dtos/ChangePasswordDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Dtos
+{
+    public class ChangePasswordDto : IDto
+    {
+        public int UserId { get; set; }
+        public string UserEmail { get; set; }
+
+        public byte[] OldPass { get; set; }
+        public byte[] NewPass { get; set; }
+    }
+}
